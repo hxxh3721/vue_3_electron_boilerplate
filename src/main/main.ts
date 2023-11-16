8578dfdbc70097c5d6ce3f7c7b0c1e55affb2239
hxxh3721 <wordvicya@gmail.com>
2023-11-16 21:41:02 +0800

    done one

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,161 +1,89 @@
-import {app, BrowserWindow, ipcMain, session} from 'electron';
-import {join} from 'path';
-const fs = require('fs');
-const path = require('path');
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+import fs from 'fs';
 
-
-
-//设置窗口图标
+// 设置窗口图标路径
 const iconPath = process.env.NODE_ENV === 'development'
   ? path.join(__dirname, 'static', 'buttons.ico')
   : path.join(app.getAppPath(), 'static', 'buttons.ico');
 
-function createWindow () {
+
+// 创建浏览器窗口的函数
+function createWindow() {
   const mainWindow = new BrowserWindow({
-    icon: iconPath,// 设置窗口图标
+    icon: iconPath, // 设置窗口图标
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
 
+  mainWindow.maximize(); // 打开窗口默认最大化
+  mainWindow.setMenu(null); // 移除菜单栏
 
-//打开窗口默认最大化
-  mainWindow.maximize();
-
-
-// 这将移除菜单栏
-  mainWindow.setMenu(null); 
-
-
-
-// 设置调试按钮
-  mainWindow.webContents.on('before-input-event', (event, input) => {
-    if (input.type === 'keyDown' && input.key === 'F12') {
-      mainWindow.webContents.openDevTools();
-    }
-  });
-
+  // 在开发模式下，加载开发服务器的URL
   if (process.env.NODE_ENV === 'development') {
     const rendererPort = process.argv[2];
     mainWindow.loadURL(`http://localhost:${rendererPort}`);
-  }
-  else {
-    mainWindow.loadFile(join(app.getAppPath(), 'renderer', 'index.html'));
+  } else {
+    // 在生产模式下，加载编译后的HTML文件
+    mainWindow.loadFile(path.join(app.getAppPath(), 'renderer', 'index.html'));
   }
 }
 
 
+// 复制文件并在文件开头添加路径信息
+function copyFileWithHeader(src, dest) {
+  fs.copyFileSync(src, dest);
+  const header = `本文件位于：${dest}\n`;
+  const content = fs.readFileSync(dest, 'utf8');
+  fs.writeFileSync(dest, header + content, 'utf8');
+}
 
-//创建窗口
+
+// 当Electron应用准备就绪后执行
 app.whenReady().then(() => {
-  createWindow();
+  createWindow(); // 创建窗口
 
-  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-    callback({
-      responseHeaders: {
-        ...details.responseHeaders,
-        'Content-Security-Policy': ['script-src \'self\'']
-      }
-    })
-  })
+  if (process.env.NODE_ENV !== 'development') {
+    const userDataPath = app.getPath('userData');
+    const destFilePath = path.join(userDataPath, 'DataReadMe.md');
+    const srcFilePath = path.join(app.getAppPath(), 'static', 'data', 'DataReadMe.md');
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+    // 生产模式下，如果文件不存在，则复制并添加路径头
+    if (!fs.existsSync(destFilePath)) {
+      copyFileWithHeader(srcFilePath, destFilePath);
     }
-  });
-});
-
-
-
-//监听关闭窗口
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-});
-
-
-
-
-
-// 辅助函数：确保文件从asar包复制到userData目录
-// src - 源文件路径
-// dest - 目标文件路径
-function ensureFileCopied(src, dest) {
-  // 检查文件是否已存在于目标路径
-  if (fs.existsSync(src) && !fs.existsSync(dest)) {
-    // 如果不存在，复制文件到目标路径
-    fs.copyFileSync(src, dest);
-
-    // 读取目标文件的内容
-    const fileContent = fs.readFileSync(dest, 'utf8');
-
-    // 创建要添加的文本行，包括目标文件的路径
-    const lineToAdd = `本文档位于: ${dest}\n`;
-
-    // 将新行和原始内容组合
-    const updatedContent = lineToAdd + fileContent;
-
-    // 写回更新后的内容到目标文件
-    fs.writeFileSync(dest, updatedContent, 'utf8');
   }
-}
+});
+
+
+
+// 当所有窗口关闭时退出应用（MacOS除外）
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // 处理读取文件的请求
 ipcMain.on('read-file', (event) => {
-  let filePath;
-  // 检查当前是否处于开发模式
-  if (process.env.NODE_ENV === 'development') {
-    // 在开发模式下，直接从项目文件夹中的路径读取文件
-    filePath = path.join(__dirname, 'static', 'data', 'DataReadMe.md');
-  } else {
-    // 在生产模式下，使用userData路径
-    const userDataPath = app.getPath('userData');
-    const destFilePath = path.join(userDataPath, 'DataReadMe.md');
-    const srcFilePath = path.join(process.resourcesPath, 'static', 'data', 'DataReadMe.md');
-    // 确保文件已从asar包中复制到userData目录
-    ensureFileCopied(srcFilePath, destFilePath);
-    // 设置文件路径为userData目录中的路径
-    filePath = destFilePath;
-  }
+  const userDataPath = app.getPath('userData');
+  const filePath = process.env.NODE_ENV === 'development'
+    ? path.join(__dirname, 'static', 'data', 'DataReadMe.md')
+    : path.join(userDataPath, 'DataReadMe.md');
 
-  // 读取文件内容
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      // 如果出现错误，向前端发送错误信息
-      console.error('读取文件出错', err);
-      event.reply('file-error', err.message);
-      return;
-    }
-    // 成功读取后，发送文件内容
-    event.reply('file-content', data);
-  });
+  // 读取文件并发送内容到渲染器进程
+  const data = fs.readFileSync(filePath, 'utf8');
+  event.reply('file-content', data);
 });
 
 // 处理写入文件的请求
 ipcMain.on('write-file', (event, updatedContent) => {
-  let filePath;
-  // 检查当前是否处于开发模式
-  if (process.env.NODE_ENV === 'development') {
-    // 在开发模式下，直接使用项目文件夹中的路径
-    filePath = path.join(__dirname, 'static', 'data', 'DataReadMe.md');
-  } else {
-    // 在生产模式下，使用userData路径
-    filePath = path.join(app.getPath('userData'), 'DataReadMe.md');
-  }
+  const userDataPath = app.getPath('userData');
+  const filePath = process.env.NODE_ENV === 'development'
+    ? path.join(__dirname, 'static', 'data', 'DataReadMe.md')
+    : path.join(userDataPath, 'DataReadMe.md');
 
   // 将更新后的内容写入文件
-  fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
-    if (err) {
-      // 如果写入过程出错，向前端发送错误信息
-      console.error('写入文件出错', err);
-      event.reply('file-error', err.message);
-      return;
-    }
-    // 写入成功后，通知前端
-    event.reply('file-write-success');
-  });
-});
+  fs.writeFileSync(filePath, updatedContent, 'utf8');
+})
--- src/main/static/data/DataReadMe.md
+++ src/main/static/data/DataReadMe.md
@@ -1,3 +1,3 @@
 # Data ReadMe
 
-This is a README file for the database. qqsb sbsb sb .99 8
+new new
