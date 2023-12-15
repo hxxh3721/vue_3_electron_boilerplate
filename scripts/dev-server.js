3d5f070a4b21bad96f41a438c6d456e6f68bfc5c
hxxh3721 <wordvicya@gmail.com>
2023-12-16 07:38:17 +0800

    unfinish

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -32,34 +32,49 @@ async function startRenderer() {
 
 // 定义启动 Electron 的异步函数
 async function startElectron() {
+    // 检查是否已经启动 Electron 进程，避免重复启动
     if (electronProcess) {
-        return;
+        return; // 如果已经启动，直接返回
     }
+    
     try {
+        // 编译 TypeScript 代码，如果出错则捕获异常
         await compileTs(Path.join(__dirname, '..', 'src', 'main'));
     } catch {
+        // 如果编译出错，输出错误信息，然后设置标志
         console.log(Chalk.redBright('无法启动 Electron，因为存在 TypeScript 错误。'));
         electronProcessLocker = false;
         return;
     }
+    
+    // 构建启动 Electron 进程所需的参数数组
     const args = [Path.join(__dirname, '..', 'build', 'main', 'main.js'), rendererPort];
+    
+    // 使用 ChildProcess.spawn 方法启动 Electron 主进程，注意方法名字虽然是子进程，但是实际启动的是electron主进程
     electronProcess = ChildProcess.spawn(Electron, args);
+    
+    // 设置标志，表示 Electron 进程已启动
     electronProcessLocker = false;
 
-    // 配置 Electron 子进程的输出
+    // 配置 Electron 主进程的输出
     electronProcess.stdout.on('data', data => {
         if (data == EOL) {
-            return;
+            return; // 忽略空数据
         }
+        // 将 Electron 主进程的标准输出写入标准输出流，并添加标识
         process.stdout.write(Chalk.blueBright(`[electron] `) + Chalk.white(data.toString()))
     });
 
-    electronProcess.stderr.on('data', data => 
+    electronProcess.stderr.on('data', data => {
+        // 将 Electron 主进程的标准错误写入标准错误流，并添加标识
         process.stderr.write(Chalk.blueBright(`[electron] `) + Chalk.white(data.toString()))
-    );
+    });
 
+    // 监听 Electron 主进程的退出事件，然后调用 stop 函数
     electronProcess.on('exit', () => stop());
+
 }
+
 
 // 重启 Electron 进程的函数
 function restartElectron() {
@@ -74,10 +89,7 @@ function restartElectron() {
     }
 }
 
-// 复制静态文件到构建目录
-function copyStaticFiles() {
-    copy('static');
-}
+
 
 // 复制文件的辅助函数
 /*
@@ -92,6 +104,14 @@ function copy(path) {
     );
 }
 
+
+// 复制静态文件到构建目录
+function copyStaticFiles() {
+    copy('static');
+}
+
+
+
 // 停止服务和退出程序的函数
 function stop() {
     viteServer.close();
@@ -100,29 +120,43 @@ function stop() {
 
 // 主函数，用于启动整个开发服务器
 async function start() {
+    // 输出启动信息
     console.log(`${Chalk.greenBright('=======================================')}`);
     console.log(`${Chalk.greenBright('Starting Electron + Vite Dev Server...')}`);
     console.log(`${Chalk.greenBright('=======================================')}`);
 
+    // 启动渲染器开发服务器，获取其配置中的端口号
     const devServer = await startRenderer();
     rendererPort = devServer.config.server.port;
 
+    // 复制静态文件到构建目录
     copyStaticFiles();
+
+    // 启动 Electron 主进程
     startElectron();
 
-    // 监听文件变动，重启 Electron 进程
+    // 监听文件变动，用于重启 Electron 进程
     const path = Path.join(__dirname, '..', 'src', 'main');
-    Chokidar.watch(path, { cwd: path }).on('change', (path) => {
-        console.log(Chalk.blueBright(`[electron] `) + `文件 ${path} 发生变动，正在重载... 🚀`);
 
-        if (path.startsWith(Path.join('static', '/'))) {
-            copy(path);
+    // 使用 Chokidar 模块创建文件变动的监听器，监视指定目录中的文件变动
+    Chokidar.watch(path, { cwd: path }).on('change', (changedFilePath) => {
+        // 打印文件变动信息，包括文件路径和提示消息
+        console.log(Chalk.blueBright(`[electron] `) + `文件 ${changedFilePath} 发生变动，正在重载... 🚀`);
+
+        // 检查变动的文件是否位于静态文件目录
+        if (changedFilePath.startsWith(Path.join('static', '/'))) {
+            // 如果是静态文件目录中的文件变动，执行以下操作：
+
+            // 复制变动的文件到构建目录，以确保最新的静态文件被更新到构建目录
+            copy(changedFilePath);
         }
 
+        // 重启 Electron 进程，以应用最新的代码和资源变动
         restartElectron();
     });
 }
 
+// 调用主函数启动开发服务器
 start();
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// 导入各种 Vite 插件和工具
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -7,26 +8,29 @@ import vuePlugin from '@vitejs/plugin-vue';
 const Path = require('path');
 const { defineConfig } = require('vite');
 
-/**
- * https://vitejs.dev/config
- */
+// Vite 配置文档：https://vitejs.dev/config
 const config = defineConfig({
+    // 设置项目的根目录
     root: Path.join(__dirname, 'src', 'renderer'),
+    // 指定静态资源目录
     publicDir: 'public',
+    // 配置开发服务器
     server: {
-        port: 8080,
+        port: 8080, // 指定服务器端口
     },
-    open: false,
+    open: false, // 是否在服务器启动时自动打开浏览器
+    // 构建配置
     build: {
-        outDir: Path.join(__dirname, 'build', 'renderer'),
-        emptyOutDir: true,
+        outDir: Path.join(__dirname, 'build', 'renderer'), // 指定输出目录
+        emptyOutDir: true, // 构建前清空输出目录
     },
+    // 插件列表
     plugins: [
-        vuePlugin(),
-        AutoImport({
+        vuePlugin(), // Vue 插件
+        AutoImport({ // 自动导入插件
           resolvers: [ElementPlusResolver()],
         }),
-        Components({
+        Components({ // Vue 组件插件
           resolvers: [ElementPlusResolver()],
         }),
     ],
