fdc9a01f6af4cae072a4424918f7dc65262e1137
hxxh3721 <wordvicya@gmail.com>
2024-03-11 16:10:13 +0800

    初始版本完成

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Customer from '../views/Customer.vue'// 导入 AddCustomer 组件
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
